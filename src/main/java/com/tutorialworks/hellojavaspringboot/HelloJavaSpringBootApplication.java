7c1e8de35d2b99383dca981becce0c84fe3f9cbe
onurkaratas <onkar.atasi@gmail.com>
2022-12-23 23:46:25 +0300

    update

--- src/main/java/com/tutorialworks/hellojavaspringboot/HelloJavaSpringBootApplication.java
+++ src/main/java/com/tutorialworks/hellojavaspringboot/HelloJavaSpringBootApplication.java
@@ -1,13 +1,26 @@
 package com.tutorialworks.hellojavaspringboot;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
 @SpringBootApplication
-public class HelloJavaSpringBootApplication {
+@Controller
+
+public class SpringHelloApplication {
+
+	@GetMapping("/")
+	public String index(final Model model) {
+		model.addAttribute("title", "Spring Boot");
+		model.addAttribute("msg", "Hello World!");
+		return "index";
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloJavaSpringBootApplication.class, args);
+		SpringApplication.run(SpringHelloApplication.class, args);
 	}
 
 }
